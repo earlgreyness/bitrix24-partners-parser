5bff74c7786ee5192cb0d655c097b21b7aa91d9c
Kirill Kondratenko <kk92@ya.ru>
2019-01-14 21:34:27 +0300

    Finish main parser

--- parse.py
+++ parse.py
@@ -1,46 +1,31 @@
 #!/usr/bin/env python3
 import csv
-import pickle
-import re
 from collections import OrderedDict
 from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime
 from pprint import pprint
 
 import attr
 import requests
 from bs4 import BeautifulSoup
-from phonenumbers import (
-    NumberParseException, PhoneNumberFormat, parse as parse_phonenumber,
-    format_number, is_possible_number, is_valid_number,
-)
 
 HOST = 'https://www.bitrix24.ru'
-DIGITS = set('0123456789+()')
 
 
 @attr.s
 class Contact:
-    name = attr.ib()
-    url = attr.ib()
-    phones = attr.ib()
+    name = attr.ib(default='')
+    url = attr.ib(default='')
+    website = attr.ib(default='')
+    phones = attr.ib(default='')
 
 
-def extract_contact(soup):
-    print(soup)
-    print('-' * 100)
-
+def extract_url(soup):
     link = soup.find('a', class_='bp-partner-list-item-name')
 
-    name = link.string
     url = HOST + link.get('href')
     phones = soup.find('div', class_='bp-partner-request-phone').string
 
-    return Contact(
-        name=name,
-        url=url,
-        phones=phones,
-    )
+    return url
 
 
 def download_partners(page):
@@ -53,22 +38,57 @@ def download_partners(page):
     return BeautifulSoup(html, 'html.parser')
 
 
+def download_detailed_info(url):
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    try:
+        name = soup.find('h2', class_='bx-pdl-header-title-name-content').string
+    except AttributeError:
+        return None
+    website = soup.find('a', class_='pt_card_hd_link').string.strip()
+
+    description = soup.find('div', class_='bx-pdl-description-contacts-content')
+
+    # print('='*100)
+    # for tag in description.find_all('p', recursive=False):
+    #     print(tag)
+
+    return Contact(
+        name=name,
+        url=url,
+        website=website,
+    )
+
+
 def main():
-    contacts = []
     page = 1
-    names = set()
+    stop = False
+    partners = OrderedDict()
 
     while True:
         soup = download_partners(page)
         divs = soup.find_all('div', class_='bp-partner-list-item-cnr')
-        new_contacts = [extract_contact(x) for x in divs]
-        new_names = {x.name for x in new_contacts}
-        if names & new_names:
+
+        for url in [extract_url(x) for x in divs]:
+            if url in partners:
+                stop = True
+                break
+            else:
+                partners[url] = None
+
+        if stop:
             break
-        contacts.extend(new_contacts)
-        names.update(new_names)
+
         print('Parsed page', page)
         page += 1
+
+    urls = list(partners.keys())
+
+    with ThreadPoolExecutor() as executor:
+        for i, contact in enumerate(executor.map(download_detailed_info, urls), start=1):
+            partners[contact.url] = contact
+            print('Done {}/{}: {}'.format(i, len(urls), contact))
+
+    contacts = [x for x in partners.values() if x is not None]
 
     pprint(contacts)
     pprint(len(contacts))
@@ -78,7 +98,7 @@ def main():
         writer = csv.writer(file)
 
         for contact in contacts:
-            writer.writerow([contact.name, contact.url, contact.phones])
+            writer.writerow([contact.name, contact.url, contact.website])
 
 
 if __name__ == '__main__':
