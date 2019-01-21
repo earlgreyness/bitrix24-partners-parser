3ba60cf5c84bf88a1e1ba03eb1d4bd5f8b7eadee
Kirill Kondratenko <kk92@ya.ru>
2019-01-21 19:40:33 +0300

    Update parser to parse more info

--- parse.py
+++ parse.py
@@ -3,6 +3,7 @@ import csv
 from collections import OrderedDict
 from concurrent.futures import ThreadPoolExecutor
 from pprint import pprint
+import pickle
 
 import attr
 import requests
@@ -13,26 +14,24 @@ HOST = 'https://www.bitrix24.ru'
 
 @attr.s
 class Contact:
-    name = attr.ib(default='')
-    url = attr.ib(default='')
-    website = attr.ib(default='')
-    phones = attr.ib(default='')
+    name = attr.ib()
+    url = attr.ib()
+    website = attr.ib()
+    phones = attr.ib()
+    email = attr.ib()
+    address = attr.ib()
 
 
 def extract_url(soup):
     link = soup.find('a', class_='bp-partner-list-item-name')
-
-    url = HOST + link.get('href')
-    phones = soup.find('div', class_='bp-partner-request-phone').string
-
-    return url
+    return HOST + link.get('href')
 
 
-def download_partners(page):
-    payload = dict(
-        ajax='Y',
-        page_n=page,
-    )
+def download_partners_page(page):
+    payload = {
+        'ajax': 'Y',
+        'page_n': page,
+    }
     resp = requests.post(HOST + '/partners/', data=payload)
     html = resp.json()['html']
     return BeautifulSoup(html, 'html.parser')
@@ -48,24 +47,37 @@ def download_detailed_info(url):
 
     description = soup.find('div', class_='bx-pdl-description-contacts-content')
 
-    # print('='*100)
-    # for tag in description.find_all('p', recursive=False):
-    #     print(tag)
+    print('='*100)
+    data = {}
+    for tag in description.find_all('p', recursive=False):
+        children = [x for x in tag if x != '\n']
+        print(children)
+        meta = children[0].string.strip()
+        if 'E-mail' in meta:
+            value = children[1].find_all('a')[0].string.strip()
+        else:
+            value = children[1].string.strip()
+        data[meta] = value
+        print(data)
+        print('>'*100)
 
     return Contact(
         name=name,
         url=url,
         website=website,
+        phones=data.get('Телефон:'),
+        email=data.get('E-mail:'),
+        address=data.get('Адрес:'),
     )
 
 
-def main():
+def download_partners():
     page = 1
     stop = False
     partners = OrderedDict()
 
     while True:
-        soup = download_partners(page)
+        soup = download_partners_page(page)
         divs = soup.find_all('div', class_='bp-partner-list-item-cnr')
 
         for url in [extract_url(x) for x in divs]:
@@ -81,6 +93,17 @@ def main():
         print('Parsed page', page)
         page += 1
 
+    return partners
+
+
+def main():
+    # partners = download_partners()
+    # with open('partners.pickle', 'wb') as file:
+    #     pickle.dump(partners, file)
+
+    with open('partners.pickle', 'rb') as file:
+        partners = pickle.load(file)
+
     urls = list(partners.keys())
 
     with ThreadPoolExecutor() as executor:
@@ -92,13 +115,15 @@ def main():
 
     pprint(contacts)
     pprint(len(contacts))
-    pprint(page)
 
     with open('bitrix24-partner-contacts.csv', 'w') as file:
         writer = csv.writer(file)
 
         for contact in contacts:
-            writer.writerow([contact.name, contact.url, contact.website])
+            writer.writerow([
+                contact.name, contact.url, contact.website, contact.email,
+                contact.address, contact.phones,
+            ])
 
 
 if __name__ == '__main__':
